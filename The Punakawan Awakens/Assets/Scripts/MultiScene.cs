46b968270faea854f9e63462ce936d2aa7a57a1a
Aryo Damar P <aryo.damar01>
2020-09-11 11:41:36 +0700

    Added Scene Loader

---
+++ The Punakawan Awakens/Assets/Scripts/MultiScene.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MultiScene : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+    }
+
+    public void UnloadScene(string sceneName)
+    {
+        SceneManager.UnloadSceneAsync(sceneName);
+    }
+
+}
